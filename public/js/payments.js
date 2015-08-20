00863af2d46f2c059612463403741264f7b4f3c2
xelad1 <xelad1@gmail.com>
2015-08-20 01:33:29 -0700

    Added major changes

--- public/js/payments.js
+++ public/js/payments.js
@@ -1,53 +1,86 @@
-angular.module('payment-App.payments',[/*'ngCookies'*/'ngAutocomplete'])
+angular.module('payment-App.payments',['ngAutocomplete'])
 
-  .controller('paymentController', function($scope/*, $cookieStore*/) {
+  .controller('paymentController', function($scope, $templateRequest/*, $cookieStore*/) {
     
     $scope.businessID;
     $scope.address;
-    $scope.isBusiness = false;
+    $scope.isBusiness = true;
+    $scope.payees = []; submit
+    $scope.people = [];
+
+    $scope.newPayee = function () {
+      this.businessID = $scope.businessID;
+      this.address = $scope.address;
+    }
 
     $scope.submit = function () {
       var text = document.getElementById("businessID").value.split(",");
       $scope.businessID = text[0];
       $scope.address = text.slice(1).join("");
-      $('.payments').appendTemplate('.Business');
+      $scope.newPayee();
+    }
+
+    
+    $scope.addPayee = function () {
+      $scope.submit();
+      if ($scope.isBusiness) {
+        $scope.payees.push(new $scope.newPayee());
+      } else {
+        $scope.people.push(new $scope.newPayee());
+      }
     }
 
     $scope.selectBusiness = function () {
-      //turns on autocomplete;
       $scope.isBusiness = true;
-      console.log("Selecting Business")
+      document.getElementById("selectPerson").className = "left attached ui button submit";
+      document.getElementById("selectBusiness").className = "left attached blue ui button submit";
     }
 
     $scope.selectPerson = function () {
-      //turns off autocomplete
       $scope.isBusiness = false;
-      console.log("Selecting Person")
-    }
-
-    $scope.fillAddress = function () {
-      // body...
-    }
-
-
-    $scope.templates = 
-      [ {
-        name: 'businessTemplate.html', url: '/templates/businessTemplate.html',
-        name: 'personTemplate.html', url: '/templates/personTemplate.html'
-      }];
-
-    $scope.template = $scope.templates[0];
-    console.log($scope.template);
-
-    return {
-
+      document.getElementById("selectBusiness").className = "left attached ui button submit";
+      document.getElementById("selectPerson").className = "left attached blue ui button submit";
     }
 
 })
+  
+  .directive("topbar", function(){
+  return {
+    restrict: "A",
+    templateUrl: '<div>{{aCard.businessID}} {{aCard.address}}</div>',
+    replace: true,
+    transclude: false,
+    scope: {
+      businessID: '=topbar'
+    }
+  }
+})
 
-  .directive('customer', function() {
-
-
-  })
+   // return {
+   //              restrict: 'A',
+   //              template: '<div>{{aCard.name}} {{aCard.phone}}</div>',
+   //              replace: true,
+   //              transclude: false,
+   //              scope: {
+   //                  aCard: '=myCard'
+   //              }
 
 //set up navbar for completing google searches
+
+// </div>
+//   <div class="BusinessName" id="name">{{businessID}}</div>
+//   <div class="Address" id="address">{{address}}</div>
+//   <button class="ui icon button" id="hoverbox">
+//     <i class="dollar icon"></i>
+//   </button>
+// </div>
+  // .directive("topbar", function($compile, $templateRequest){
+  // return function(scope, element, attrs){
+  //   element.bind("click", function(){
+  //     $templateRequest("templates/businessTemplate.html").then(function(html){
+  //     var template = angular.element(html);
+  //     element.append(template);
+  //     $compile(template)(scope);
+  //  });
+  //   });
+  // };
